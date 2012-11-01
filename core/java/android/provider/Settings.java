88ccbe19c584c20855c2ca635df0990e0b5e84b2
fishears <f1she4rs@gmail.com>
2012-11-01 14:49:06 +0000

    SystemUI: Centre clock (part 2/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2461,7 +2461,8 @@ public final class Settings {
          * Whether to show the clock in status bar
          * of the stock battery icon
          * 0: don't show the clock
-         * 1: show the clock
+         * 1: right side clock
+         * 2: centre clock
          * default: 1
          * @hide
          */
