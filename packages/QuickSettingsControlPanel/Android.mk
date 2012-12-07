8b7cc4a839d83cec909ca058d773faab5a4c5139
fishears <f1she4rs@gmail.com>
2012-12-07 17:57:50 +0000

    Revert "Fix: Quicksettings mod not compiling"
    This reverts commit f2990fd5368378df56108ffe8f4debf87397fd53.

--- packages/QuickSettingsControlPanel/Android.mk
+++ packages/QuickSettingsControlPanel/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := QuickSettingsControlPanel
-LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
